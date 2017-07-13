f548436499d6990c26384c1774914fc92dd842af
Himanish <hsetia94@gmail.com>
2017-07-13 16:57:16 +0530

    Notification Analytics
    Signed-off-by: Himanish <hsetia94@gmail.com>

---
+++ js/controller/main.js
@@ -0,0 +1,191 @@
+notificationSample.controller('mainController',function($scope,$location,notificationService,$filter){
+	App42.setEventBaseUrl("https://in-analytics.shephertz.com/cloud/1.0/");
+	App42.initialize("b9eb670c3d1c4e666982dcccd58515f82a1096879849a6c76bada4b388c3af96", "1c1d184fdcf33ae29c24cffdf653b8e69e8c89eeb6813791f3785c6278f291ed");  
+	App42.enableEventService(true);  
+	App42.enableAppStateEventTracking(true);
+	var eventService = new App42Event();  
+	$scope.notificationType = "success"
+
+
+	// Get the modal
+var modal = document.getElementById('usernameDialog');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	var eventName = "Skip_Login"
+	var date = new Date();
+	var currDate = $filter('date')(new Date(), 'dd, MMMM yyyy');
+	var currTime = $filter('date')(new Date(), 'hh:mm:ss a');
+	var dateTime = currDate +" "+ currTime
+
+	var properties = { "Email":"Guest User",
+	"Creation Date":dateTime }  
+	App42.setLoggedInUser("Guest"); 
+
+
+	eventService.setLoggedInUserProperties(properties, {  
+    success: function (object) {  
+        var eventObj = JSON.parse(object)  
+        console.log("Success is : " , eventObj);  
+    },  
+    error: function (error) {  
+        console.log("Exception is : " , error);  
+    }  
+}); 
+
+	$scope.trackEvent(eventName,properties)
+
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+$scope.init = function(){
+ modal.style.display = "block";
+}
+
+$scope.init();
+
+
+$scope.submitUsername = function(){
+
+	var eventName = "Login"
+	var user
+
+	var date = new Date();
+	var currDate = $filter('date')(new Date(), 'dd, MMMM yyyy');
+	var currTime = $filter('date')(new Date(), 'hh:mm:ss a');
+	var dateTime = currDate +" "+ currTime
+
+	var properties = { "Email":$scope.username,
+	"Creation Date":dateTime }  
+
+
+	if($scope.userName != '' && $scope.userName != undefined && $scope.userName != null){
+		user = $scope.userName
+	}
+	else{
+		user = "Guest"
+	}
+
+
+
+	App42.setLoggedInUser(user); 
+
+
+	eventService.setLoggedInUserProperties(properties, {  
+    success: function (object) {  
+        var eventObj = JSON.parse(object)  
+        console.log("Success is : " , eventObj);  
+    },  
+    error: function (error) {  
+        console.log("Exception is : " , error);  
+    }  
+});  
+
+$scope.trackEvent(eventName,properties)
+
+  modal.style.display = "none";
+}
+
+$scope.notify = function(){
+
+console.log("$scope.notificationType ",$scope.notificationType)
+
+notificationService.notify({
+	title: $scope.title?$scope.title:"",
+	title_escape: false,
+	text: $scope.message?$scope.message:"",
+	text_escape: false,
+	styling: "bootstrap3",
+	type: $scope.notificationType,
+	icon: true
+});
+
+
+var properties = { "Title":$scope.title?$scope.title:"",
+	"Message":$scope.message?$scope.message:"",
+	"Notification Type":$scope.notificationType }  
+	
+	var eventName = "Notification"
+	
+
+	$scope.trackEvent(eventName,properties)
+	$scope.notificationType = "success"
+	$scope.title = ""
+	$scope.message = ""
+	
+}
+
+
+$scope.confirmDialog = function() {
+
+notificationService.notify({
+	title: $scope.title?$scope.title:"",
+	text: $scope.message?$scope.message:"",
+	hide: false,
+	confirm: {
+		confirm: true
+	},
+	buttons: {
+		closer: false,
+		sticker: false
+	},
+	history: {
+		history: false
+	}
+}).get().on('pnotify.confirm', function() {
+	alert('Ok, cool.');
+}).on('pnotify.cancel', function() {
+	alert('Oh ok. Chicken, I see.');
+});
+
+var properties = { "Title":$scope.title?$scope.title:"",
+	"Message":$scope.message?$scope.message:""}  
+
+	if($scope.notifySubmit == "Notify"){
+		properties["Notification Type"] = $scope.notificationType
+	}
+
+	
+	var eventName = "Confirm_Dialog"
+	
+
+	$scope.trackEvent(eventName,properties)
+	$scope.notificationType = "success"
+	$scope.title = ""
+	$scope.message = ""
+}
+
+
+
+$scope.trackEvent = function(eventName,properties){ 
+	// App42.setLoggedInUser($scope.userName); 
+	// var eventService = new App42Event();    
+	eventService.trackEvent(eventName, properties, {        
+    success: function (object) {  
+        var eventObj = JSON.parse(object)  
+        console.log("Success is : " + eventObj);  
+    },  
+    error: function (error) {  
+        console.log("Exception is : " + error);  
+    }  
+});  
+
+}   
+
+})
---
+++ js/route/route.js
@@ -0,0 +1,9 @@
+var notificationSample = angular.module('notificationTemplate',['ngRoute','ngAnimate','jlareau.pnotify']);
+
+notificationSample.config(['$routeProvider','$httpProvider',function($routeProvider,$httpProvider){
+	$routeProvider
+	// .when('/',{
+	// 		templateUrl : 'index.html',
+	// 		controller : 'mainController'
+	// 	})
+}])
